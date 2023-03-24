a7b0e5c1cbdd68572281aa1bfd7bce0e294ca3c1
Soualemmouhssin <elseniormouhssin@gmail.com>
2023-03-24 06:01:15 -0700

    test

---
+++ 0x04-more_functions_nested_loops/alx-low_level_programming.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+int _isupper(int c);
+int _isupper(int c){
+       	if (c>64 && c<91)
+   	{
+       		return 1;   
+	}	
+	else
+   {
+         return 0
+   }
+}
